0f4da3b4f42d7f6179613f8927ee27d0d4417621
ericfeunekes <ericwill.f@gmail.com>
2023-01-14 16:18:09 +0000

    save

--- src/base_schema/constructors/dataclass.py
+++ src/base_schema/constructors/dataclass.py
@@ -1,15 +1,24 @@
 """Constructor for dataclasses."""
 from dataclasses import make_dataclass
 from functools import partial
+from typing import Any, Tuple
 
-def get_dataclass_field(field: dict, type_definitions: dict):
+def get_dataclass_field(field: dict, type_definitions: dict) -> Tuple[str, Any]:
+    """Create a dataclass field from a field definition.
+
+    Args:
+        field (dict): The field definition.
+        type_definitions (dict): The type definitions.
+
+    Returns:
+        dataclasses.Field: The dataclass field.
+    """
     name = field["name"]
     property_type = field["type"]
 
-    constructor = type_definitions[(property_type, "dataclass")]    
+    constructor = type_definitions[(property_type, "dataclass")]
 
-    field = constructor(name)
-    dataclass_fields.append(field)
+    return constructor(name)
 
 def create_dataclass(definition: dict, type_definitions: dict):
     """Create a dataclass type from a definition.
@@ -24,6 +33,7 @@ def create_dataclass(definition: dict, type_definitions: dict):
         name = definition["name"]
     except KeyError as exc:
         raise ValueError("Dataclass definition must have a name.") from exc
+
     partial_get_field = partial(get_dataclass_field, type_definitions=type_definitions)
     dataclass_fields = [partial_get_field(field) for field in definition["fields"]]        
 
@@ -31,5 +41,3 @@ def create_dataclass(definition: dict, type_definitions: dict):
         cls_name=name, 
         fields=dataclass_fields
         )
-    
-    #
